e0be9131200c46f1aa49ade2c74dc6208f6152cd
齐政浩 <zhenghao.qi@renren-inc.com>
2015-10-09 17:05:49 +0800

    增加shader

--- Android_Knowledges/src/com/bruce/android/knowledges/MyActivity.java
+++ Android_Knowledges/src/com/bruce/android/knowledges/MyActivity.java
@@ -1,10 +1,18 @@
 package com.bruce.android.knowledges;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.ListView;
+import com.bruce.android.knowledges.Activities.TestShaderActivity;
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity implements View.OnClickListener{
+
+    private Context mContext = null;
     /**
      * Called when the activity is first created.
      */
@@ -13,7 +21,64 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        initViews();
+        initData();
+    }
+
+    private void initData() {
+        mContext = MyActivity.this;
+    }
+
+    private void initViews() {
 
     }
 
+
+    private AlphaAnimation mHideAnimation= null;
+    private AlphaAnimation mShowAnimation= null;
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_open_test_shader_btn:
+                startActivity(new Intent(mContext, TestShaderActivity.class));
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
+    /**
+     * View渐隐动画效果
+     */
+    private void setHideAnimation(View view, int duration) {
+        if (null == view || duration < 0) {
+            return;
+        }
+        if (null != mHideAnimation) {
+            mHideAnimation.cancel();
+        }
+        mHideAnimation = new AlphaAnimation(1.0f, 0.0f);
+        mHideAnimation.setDuration(duration);
+        mHideAnimation.setFillAfter(true);
+        view.startAnimation(mHideAnimation);
+    }
+
+    /**
+     * View渐现动画效果
+     */
+    private void setShowAnimation(View view, int duration) {
+        if (null == view || duration < 0) {
+            return;
+        }
+        if (null != mShowAnimation) {
+            mShowAnimation.cancel();
+        }
+        mShowAnimation = new AlphaAnimation(0.0f, 1.0f);
+        mShowAnimation.setDuration(duration);
+        mShowAnimation.setFillAfter(true);
+        view.startAnimation(mShowAnimation);
+    }
 }
