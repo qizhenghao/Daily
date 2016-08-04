60088a0844ba0c894f5cf9f0273d1f972ba2b75a
qizhenghao <qizhenghao@mymac.local>
2016-08-04 21:21:03 +0800

    TestPow

--- JAVA_Knowledges/src/com/bruce/java/math/TestPow.java
+++ JAVA_Knowledges/src/com/bruce/java/math/TestPow.java
@@ -4,4 +4,18 @@ package com.bruce.java.math;
  * Created by qizhenghao on 16/6/21.
  */
 public class TestPow {
+
+    public static void main(String[] args) {
+        double sumPrice = 8864 * 78.94d;
+        double monthRate = 4.9d/100/12;
+        double monthCount = 15*12;
+
+        double everyMonthPay = 480000 * monthRate * (Math.pow((1 + monthRate), monthCount) / ((Math.pow((1 + monthRate), monthCount) - 1)));
+
+        System.out.println(everyMonthPay);
+
+        float f1 = (float) Math.pow(1.005, 360);
+
+        System.out.println(f1);
+    }
 }
